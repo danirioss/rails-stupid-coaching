4bc7c55a9a5476c5adcfbf763b3a18b7c30ef95a
Daniela Rios <danirioscampelo@gmail.com>
2023-02-14 16:44:09 -0300

    minitest and capybara working well

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -12,21 +12,25 @@ class QuestionsTest < ApplicationSystemTestCase
   # end
   test "saying Hello yields a grumpy response from the coach" do
     visit ask_url
-    fill_in "question", with: "Hello"
+    fill_in "user_question", with: "Hello"
     click_on "Ask"
-
     assert_text "I don't care, get dressed and go to work"
+    save_and_open_screenshot
   end
 
   test "asking anything also gets a grumpy response from the coach" do
     visit ask_url
-    fill_in "question", with: "Hello?"
+    fill_in "user_question", with: "Hello?"
     click_on "Ask"
+    assert_text "Silly question, get dressed and go to work!"
+    save_and_open_screenshot
   end
 
   test "saying I am going to work gets Great as an answer" do
     visit ask_url
-    fill_in "question", with: "I am going to work"
+    fill_in "user_question", with: "I am going to work"
     click_on "Ask"
+    assert_text "Great"
+    save_and_open_screenshot
   end
 end
